7ca2c305e4453168f91afe340c972232c72b93f7
Dominik Beran <d.beran27@gmail.com>
2023-10-04 10:12:17 +0200

    create navigation

--- webpack.config.js
+++ webpack.config.js
@@ -67,11 +67,11 @@ module.exports = {
 				use: ['html-loader']
 			},
 			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				test: /\.(png|svg|jpg|jpeg|gif|svg)$/i,
 				type: 'asset/resource'
 			},
 			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
 				use: [
 					{
 						loader: 'file-loader',
