b013357dd341b56376cc1c622aede74bf06d2b7c
Dominik Beran <d.beran27@gmail.com>
2023-10-04 09:13:00 +0200

    webpack file-loader config, add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -62,8 +62,6 @@ module.exports = {
 
 				]
 			},
-
-
 			{
 				test: /\.html$/i,
 				use: ['html-loader']
@@ -72,6 +70,18 @@ module.exports = {
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource'
 			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			}
 		]
 	}
 }
